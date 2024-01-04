cc3deb6c2cc4d8b075886d778e41e8ba63b7174d
1garo <alevardai427@gmail.com>
2024-01-04 11:57:53 -0300

    wip

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/1garo/monkey-lang/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -24,3 +26,21 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) NexToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Literal: string(ch),
+		Type: tokenType,
+	}
+}
